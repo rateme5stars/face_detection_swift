494eac0c8eeae800beb04b74944021ba6041b034
rateme5stars <dzungardan@gmail.com>
2023-02-25 15:59:13 +0100

    Initial Commit

---
+++ yolov5swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yolov5swift
+//
+//  Created by Dzung Ngo on 25/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
